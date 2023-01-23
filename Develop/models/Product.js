bc16ed513a778e6f20515ba2457d6b49bf415934
slthurman01 <slthurman01@gmail.com>
2023-01-23 14:07:58 -0600

    Adds PrductTag model columns

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -4,7 +4,7 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
@@ -31,13 +31,13 @@ Product.init(
       isNumeric: true
     },
     category_id: {
-    type: DataTypes.INTEGER,
-    references: {
-      model: 'category',
-      key: 'id'
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
     }
-  }
-},
+  },
   {
     sequelize,
     timestamps: false,
