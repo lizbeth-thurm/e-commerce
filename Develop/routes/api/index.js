984e8a67b44fc4d8b752224942a9435928b98538
slthurman01 <slthurman01@gmail.com>
2023-01-23 15:09:40 -0600

    removes associations from index routes and moves to index model

--- Develop/routes/api/index.js
+++ Develop/routes/api/index.js
@@ -4,19 +4,6 @@ const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
 const tagRoutes = require('./tag-routes');
 
-Product.belongsTo(Category, {});
-
-Category.hasMany(Product, {});
-
-Product.belongsToMany(Tag, {
-    through: {
-        model: ProductTag,
-        unique: false
-    }
-});
-
-Tag.belongsToMany(Product, {});
-
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
 router.use('/tags', tagRoutes);
